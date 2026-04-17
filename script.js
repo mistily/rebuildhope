23626d1de6c7fa6184f9ad1a86545654014c4dfc
mistily <kutasi_beata@yahoo.com>
2026-04-17 20:16:57 +0300

    Some advances

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const tear = document.getElementById("tear");
 const story = document.getElementById("story");
 const carouselx = story.getElementsByTagName('div')[0];;
 const galleryOpenBtn = document.getElementById('open-disaster');
+const galleryClose = document.getElementById('detail-carousel').children[0].children[0];
+const leftSwipe = document.getElementById('car-left');
+const rightSwipe = document.getElementById('car-right');
 // Activate story on click
 tear.addEventListener("click", () => {
   story.classList.add("active");
@@ -12,6 +15,12 @@ carouselx.addEventListener("click", () => {
 });
 galleryOpenBtn.addEventListener("click", () => {
   document.getElementsByClassName('carousel-container')[0].style.display = 'flex';
+  let dimsRef = document.getElementById("detail-carousel").getBoundingClientRect().height;
+  document.getElementById("bg1-carousel").style.height = dimsRef + "px";
+  document.getElementById("bg2-carousel").style.height = dimsRef + "px";
+});
+galleryClose.addEventListener("click", () => {
+  document.getElementsByClassName('carousel-container')[0].style.display = 'none';
 });
 document.querySelectorAll(".card").forEach((card) => {
   const inner = card.querySelector(".card-inner");
@@ -35,6 +44,22 @@ document.querySelectorAll(".card").forEach((card) => {
   card.addEventListener("mouseleave", () => {
     inner.style.transform = "rotateX(0deg) rotateY(0deg)";
   });
+});
+leftSwipe.addEventListener("click", () => {
+  let slide = document.getElementsByClassName("slide active")[0];
+  let prevSlide = slide.previousElementSibling;
+  if(prevSlide!=null && prevSlide!=undefined && prevSlide.nextElementSibling.classList.contains("slide")) {
+    slide.classList.remove("active");
+    prevSlide.classList.add("active");
+  }
+});
+rightSwipe.addEventListener("click", () => {
+  let slide = document.getElementsByClassName("slide active")[0];
+  let nextSlide = slide.nextElementSibling;
+  if(nextSlide!=null && nextSlide!=undefined && nextSlide.classList.contains("slide")) {
+    slide.classList.remove("active");
+    nextSlide.classList.add("active");
+  }
 });
 // Share links
 const url = encodeURIComponent(window.location.href);
